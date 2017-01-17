29a392ccedcea5229fbea71e8d6fd6281d3d3b0d
Aecio Lima <aecio.lima@suportesaude.net>
2017-01-17 18:01:20 -0200

    Update shell to include package path

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!bin/bash
 #run java code from command line
 
+src="src/"
+
 if [ $# -eq 0 ]
 	then echo "Missing class file"
 fi
 
 echo "Compiling..." $1
-javac $1.java && java $1
+javac $src$1.java && java $src$1
 echo "Running..."
